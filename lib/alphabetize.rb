d592c6b9a27df4abd05379995512e979c3ec52d0
Spencer Lindemuth <lindemut@gmail.com>
2019-04-09 16:02:23 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+require 'pry'
+
 def alphabetize(arr)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   # code here
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(alphabet, ascii)}
 end
