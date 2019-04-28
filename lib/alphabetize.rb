4d62f4ab3edb2695bc3dd9883b51a2287fddd1bb
Greg Morgan <gregory.morgan@flatironschool.com>
2019-04-27 19:20:16 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,6 @@ require "pry"
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase| 
-    # binding.pry
     phrase.split("").map do |char|
       esperanto_alphabet.index(char)
     end
